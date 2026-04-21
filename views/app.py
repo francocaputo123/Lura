8feacb90b433c59d8bb332bb6e15e81740c0de53
Ezequiel <martinmonzon200020@gmail.com>
2026-04-21 09:03:38 -0300

    removed screeninfo cuz the teacher is a mother fucker piece of shit

--- views/app.py
+++ views/app.py
@@ -1,23 +1,17 @@
 import tkinter as tk
-from screeninfo import get_monitors
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title = "Lura"
 
-        # Ezequiel tiene dos monitores, por lo que hay que obtener por lo menos el monitor principal.
-        monitor = get_monitors()[0]
-
-        # obtenemos la longitud y la altura del monitor principal, luego, obtenemos la posición en la que deberá aparecer la ventana (idealmente es en el centro, por algo se divide)
-        self.width = int(monitor.width * 0.6)
-        self.height = int(monitor.height * 0.6)
-        self.position_x = monitor.x + (monitor.width - self.width) // 2
-        self.position_y = monitor.y + (monitor.height - self.height) // 2
+        # obtenemos la longitud y la altura del monitor principal, (la longitud es una proporción de la altura total)
+        self.width = int(self.winfo_screenheight() * 0.9)
+        self.height = int(self.winfo_screenheight() * 0.6)
 
         tk.Label(self, text="Bienvenido a Lura", font=("Arial", 18)).pack(pady=20)
 
-        self.geometry(f"{self.width}x{self.height}+{self.position_x}+{self.position_y}")
+        self.geometry(f"{self.width}x{self.height}")
 
     def show_frame(self, frame_class, **kwargs):
         """ Básicamente remplaza el frame actual por uno nuevo. """
